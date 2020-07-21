938121ba89b2a98af818dc29e3fabf9624105564
Ishanka Ranatunga <ishan.ranatunga@gmail.com>
2020-07-21 17:59:25 +0530

    * heading and selectable icon row

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,8 +7,66 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.hiking,
+    FontAwesomeIcons.biking
+  ];
+
+  Widget _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+            color: _selectedIndex == index
+                ? Theme.of(context).accentColor
+                : Color.fromRGBO(231, 236, 239, 1),
+            borderRadius: BorderRadius.circular(30)),
+        child: Icon(
+          _icons[index],
+          size: 25,
+          color: _selectedIndex == index
+              ? Theme.of(context).primaryColor
+              : Color.fromRGBO(179, 193, 196, 1),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: ListView(
+        padding: EdgeInsets.symmetric(vertical: 30),
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 100),
+            child: Text(
+              'What you would like to find?',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: _icons
+                .asMap()
+                .entries
+                .map((MapEntry mapEntry) => _buildIcon(mapEntry.key))
+                .toList(),
+          )
+        ],
+      )),
+    );
   }
 }
