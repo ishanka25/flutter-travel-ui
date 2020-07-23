8b4c0f169759771f9782b05476656d7c4e56175e
Ishanka Ranatunga <ishan.ranatunga@gmail.com>
2020-07-23 17:25:58 +0530

    * destination screen

---
+++ lib/screens/destination.screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:travel_ui/models/destination.model.dart';
+
+class DestinationScreen extends StatefulWidget {
+  final Destination destination;
+  DestinationScreen({this.destination});
+  @override
+  _DestinationScreenState createState() => _DestinationScreenState();
+}
+
+class _DestinationScreenState extends State<DestinationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                          blurRadius: 6)
+                    ]),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Image(
+                    image: AssetImage(widget.destination.imgURL),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        iconSize: 30,
+                        color: Colors.black,
+                        onPressed: () => Navigator.pop(context)),
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.search),
+                          iconSize: 30,
+                          color: Colors.black,
+                          onPressed: () => print('Searching....'),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.sort),
+                          iconSize: 30,
+                          color: Colors.black,
+                          onPressed: () => print('Sorting....'),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
