c8dffe635f8935abe1652583f0e16209831655ae
Ishanka Ranatunga <ishan.ranatunga@gmail.com>
2020-07-23 16:43:21 +0530

    * added hotel carousel

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travel_ui/widgets/destination_carousel.dart';
+import 'package:travel_ui/widgets/hotel_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -67,7 +68,9 @@ class _HomeScreenState extends State<HomeScreen> {
                 .toList(),
           ),
           SizedBox(height: 20),
-          DestinationCarousel()
+          DestinationCarousel(),
+          SizedBox(height: 20),
+          HotelCarousel()
         ],
       )),
     );
